08785d7109658957c1336924ca6ddfb32259e471
PirasL <leo.piras@outlook.com>
2025-09-25 11:07:34 +0200

    feat: adding config creator function

--- cmd/api/config.go
+++ cmd/api/config.go
@@ -17,3 +17,44 @@ type rateLimiterConfig struct {
 	rps     int
 	burst   int
 }
+
+func newJWTConfig() (*jwtConfig, error) {
+	secret, err := getRequiredStringEnv("JWT_SECRET")
+	if err != nil {
+		return nil, err
+	}
+
+	return &jwtConfig{
+		secret: *secret,
+	}, nil
+}
+
+func newRateLimiterConfig() *rateLimiterConfig {
+	return &rateLimiterConfig{
+		enabled: getOptionalBoolEnv("RATE_LIMITER_ENABLED", true),
+		rps:     getOptionalIntEnv("RATE_LIMITER_RPS", 5),
+		burst:   getOptionalIntEnv("RATE_LIMITER_BURST", 10),
+	}
+}
+
+func newServiceConfig() (*serviceConfig, error) {
+
+	servicePort := getOptionalIntEnv("SERVICE_PORT", 8080)
+	grpcPort := getOptionalIntEnv("SERVICE_GRPC_PORT", 8080)
+
+	rateLimiterConfig := newRateLimiterConfig()
+
+	jwtConfig, err := newJWTConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	serviceConfig := &serviceConfig{
+		servicePort:       servicePort,
+		gRPCPort:          grpcPort,
+		rateLimiterConfig: rateLimiterConfig,
+		jwtConfig:         jwtConfig,
+	}
+
+	return serviceConfig, nil
+}
